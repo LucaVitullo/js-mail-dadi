bca707d5daf56093512181ecea6a65519290244c
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-09 15:36:41 +0100

    primo es

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 /*Mail
+
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
+*/
+let email = prompt("Qual'è la tua email?").toLowerCase()
+let emailUsers = ["luca@gmail.com", "giovanni@gmail.com","pietro@gmail.com"]
+
+console.log("La tua email è " + email);
+
+for (let i = 0; i < emailUsers.length; i++){
+
+    if(email === emailUsers[i]){
+        console.log("email verificata")
+    }
+    else(email !== emailUsers);{
+        console.log("email inesistente")
+    }
+}
+
+
