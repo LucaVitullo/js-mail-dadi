5d1c6ebe1064874133e6644415efe4a6c9e5abe1
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-09 17:26:52 +0100

    added validEmail

--- js/script.js
+++ js/script.js
@@ -4,18 +4,21 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
-let email = prompt("Qual'è la tua email?").toLowerCase()
-let emailUsers = ["luca@gmail.com", "giovanni@gmail.com","pietro@gmail.com"]
-
+let email = prompt("Qual'è la tua email?").toLowerCase();
+let emailUsers = ["luca@gmail.com", "giovanni@gmail.com","pietro@gmail.com"];
+let validEmail = "";
 console.log("La tua email è " + email);
 
 for (let i = 0; i < emailUsers.length; i++){
 
     if(email === emailUsers[i]){
         console.log("email verificata")
+        validEmail = true;
+
     }
     else{
         console.log("email inesistente")
+        validEmail = false;
 
     }
     
